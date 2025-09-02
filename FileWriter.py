f9777d9e638d98ecde4b905087458cbc1e46f72a
moyshi <moyshi.deutsch@gmail.com>
2025-09-02 15:13:43 +0300

    fix FileWriter

--- FileWriter.py
+++ FileWriter.py
@@ -5,7 +5,6 @@ class IWriter:
 	def __init__(self, file_name: str):
 		self.file_name = file_name
 
-
 	def send_data(self, data: str, machine_name: str) -> None:
 		with open(self.file_name, 'a') as file:
 			file.write(f'****{self.time_taker()}****\n')
